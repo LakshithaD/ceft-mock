3563e225e1b97f56229bca7c30175ab466e9ced7
Lakshitha Matarage <lnalindanath@gmail.com>
2025-01-28 09:03:34 +0530

    added new changes

---
+++ boc-interface/src/main/java/com/boc/mock/BocInterface.java
@@ -0,0 +1,12 @@
+package com.boc.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BocInterface {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BocInterface.class, args);
+    }
+}
